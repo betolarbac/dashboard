fda478065516b1b8d979654e9df6b0701a618310
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2023-08-30 21:09:27 -0300

    pagina de login do projeto pegando os dados do db e verificando o user e type

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require_once 'includes/db.php';
+
+if (isset($_POST['login'])) {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['username'] = $username;
+            $_SESSION['type'] = $row['type'];
+            header("Location: dashboard.php");
+        } else {
+            echo "Senha incorreta!";
+        }
+    } else {
+        echo "Usuário não encontrado!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+</head>
+
+<body>
+    <h2>Login</h2>
+    <form method="post" action="">
+        <input type="text" name="username" placeholder="Nome de usuário" required><br>
+        <input type="password" name="password" placeholder="Senha" required><br>
+        <input type="submit" name="login" value="Entrar">
+    </form>
+</body>
+
+</html>
